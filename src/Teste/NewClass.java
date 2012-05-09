ef590740dff8e455a0c2c12a76c9b5c15bc134b3
ANDRE <ANDRE@ANDRE-PC>
2012-05-09 12:21:12 -0300

    ADICIONADO LEITURA DO COMPONENTE PELO CLIQUE DO MOUSE

---
+++ src/Teste/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Teste;
+
+/**
+ *
+ * @author ANDRE
+ */
+public class NewClass {
+    
+}
